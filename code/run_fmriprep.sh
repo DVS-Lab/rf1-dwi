8501d94b9e92b98b16aeddef896730f3f87916cc
DVSneuro <david.v.smith@temple.edu>
2024-07-07 13:40:47 -0400

    only run missing subs

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -6,10 +6,19 @@ scriptdir=$maindir/code
 
 mapfile -t myArray < ${scriptdir}/sublist.txt
 
+# make array based on what actually needs to be run
+for sub in ${myArray[@]}; do
+	surf=${maindir}/derivatives/fmriprep/${sub}/anat/${sub}_hemi-R_space-fsLR_desc-msmsulc_sphere.surf.gii
+	html=${maindir}/derivatives/fmriprep/${sub}.html
+	if [ ! -e $surf ] || [ ! -e $html ]; then
+		missingSubs+=( $sub )
+	fi
+done
+
 ntasks=4
 counter=0
-while [ $counter -lt ${#myArray[@]} ]; do
-	subjects=${myArray[@]:$counter:$ntasks}
+while [ $counter -lt ${#missingSubs[@]} ]; do
+	subjects=${missingSubs[@]:$counter:$ntasks}
 	echo $subjects
 	let counter=$counter+$ntasks
 	qsub -v subjects="${subjects[@]}" fmriprep.qsub
--- code/run_qsiprep.sh
+++ code/run_qsiprep.sh
@@ -6,10 +6,19 @@ scriptdir=$maindir/code
 
 mapfile -t myArray < ${scriptdir}/sublist.txt
 
+# make array based on what actually needs to be run
+for sub in ${myArray[@]}; do
+	dwi=${maindir}/derivatives/qsiprep/${sub}/dwi/${sub}_space-T1w_desc-preproc_dwi.nii.gz
+	html=${maindir}/derivatives/qsiprep/${sub}.html
+	if [ ! -e $dwi ] || [ ! -e $html ]; then
+		missingSubs+=( $sub )
+	fi
+done
+
 ntasks=4
 counter=0
-while [ $counter -lt ${#myArray[@]} ]; do
-	subjects=${myArray[@]:$counter:$ntasks}
+while [ $counter -lt ${#missingSubs[@]} ]; do
+	subjects=${missingSubs[@]:$counter:$ntasks}
 	echo $subjects
 	let counter=$counter+$ntasks
 	qsub -v subjects="${subjects[@]}" qsiprep.qsub
